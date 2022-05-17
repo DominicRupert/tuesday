202b482ebeeba0889be3d5da5bd48dd4ef1a9be0
big-daddy-dom <dominicrupert@gmail.com>
2022-05-17 14:54:30 -0600

    package

--- main.js
+++ main.js
@@ -3,118 +3,178 @@ const allPackages = [
     heavy: true,
     priority: false,
     fragile: false,
+    big: true,
     to: "Harrington",
     trackingNumber: "1324kjs",
-    image: "/mgs.png",
+    illegal: true,
   },
   {
     heavy: false,
     priority: true,
     fragile: true,
+    big: false,
     to: "Mark",
     trackingNumber: "1325sdk",
-    image: "/mgs.png",
+    illegal: false,
   },
   {
     heavy: true,
     priority: false,
     fragile: true,
+    big: true,
     to: "Mick",
     trackingNumber: "jffd147",
-    image: "/mgs.png",
+    illegal: true,
   },
   {
     heavy: false,
     priority: false,
     fragile: false,
+    big: true,
     to: "Jake",
     trackingNumber: "acdc145",
-    image: "/mgs.png",
+    illegal: false,
   },
-  {
-    heavy: true,
-    priority: true,
-    fragile: true,
-    to: "Brittany",
-    image: "/mgs.png",
-  },
+  { heavy: true, priority: true, fragile: true, big: false, to: "Brittany", illegal: true },
   {
     heavy: false,
     priority: true,
     fragile: false,
+    big:false,
     to: "Zach",
     trackingNumber: "8081baz",
-    image: "/mgs.png",
+    illegal: false,
   },
   {
     heavy: true,
     priority: false,
     fragile: true,
+    big:false,
     to: "Jeremy",
     trackingNumber: "suz2367",
-
-    image: "/mgs.png",
+    illegal: true,
+  },
+  {
+    heavy: false,
+    priority: true,
+    fragile: true,
+    big:false,
+    to: "Yugi",
+    trackingNumber: "1325sdk",
+    illegal: true,
+  },
+  {
+    heavy: false,
+    priority: true,
+    fragile: true,
+    big:true,
+    to: "Josh",
+    trackingNumber: "1325sdk",
+    illegal: false,
+  },
+  {
+    heavy: true,
+    priority: false,
+    fragile: true,
+    big: false,
+    to: "Dom",
+    trackingNumber: "1325sdk",
+    illegal: false,
+  },
+  {
+    heavy: false,
+    priority: true,
+    fragile: false,
+    big: true,
+    to: "Bungus",
+    trackingNumber: "1325sdk",
+    illegal: true,
   },
 ];
-let realPackages = [];
-let choices = 0;
-let missingPackage = {};
+
+let activePackages = allPackages;
+let correctPackage = null;
+let guesses = 0;
 
 function startSort() {
-  const index = Math.floor(Math.random() * (allPackages.length - 1));
-  missingPackage = allPackages[index];
-  console.log(missingPackage);
-  realPackages[index]
-  choices = 0;
-  enableAllButtons();
-  drawPackages();
+  let index = Math.floor(Math.random() * activePackages.length);
+  console.log(index);
+  activePackages[index].correct = true;
+  correctPackage = activePackages[index];
 }
 
-function enableAllButtons() {
-  let buttons = document.getElementsByTagName("button");
-  for (let i = 0; i < buttons.length; i++) {
-    const button = buttons[i];
-    button.disabled = false;
-  }
-}
 function drawPackages() {
   let template = "";
-  // if(realPackages.length>1, )
-  for (let i = 0; i < realPackages.length; i++) {
-    const package = realPackages[i];
+  activePackages.forEach((package) => {
     template += `
-        
-        <div class="col-4 p-3">
-        <div class="bg-dark text-white rounded text-center package-card" onclick="pick('${missingPackage}')">
-        <img class="object-fit rounded-top" src="${package.image}" alt="">
-        <p><b>${package.to}</b></p>
-        
-        </div>
-        </div>
-        
-        
-        `;
-  }
-  document.getElementById("package").innerHTML = template;
-  document.getElementById("choices").innerText = choices.toString();
-}
-function choose(attribute) {
-  choices++;
+  <div class="col-md-3 text-white " onclick="choose('${package.to}')">
+  <img src="/mgs.png" class="img-fluid mgs rounded-2 " alt="">
+  <p class="text-center py-2"><b>${package.to}</b></p>
+  </div>
+  `;
+  });
 
-  realPackages = allPackages.filter(
-    (p) => p[attribute] == missingPackage[attribute]
-  );
-  document.getElementById(attribute).disabled = false;
+  document.getElementById("packages").innerHTML = template;
+}
+function drawScore() {
+  document.getElementById("guess-count").innerHTML =
+    "<h3 class='text-light'>current guesses: </h3>" + guesses;
+}
+function guessWeight() {
+  let heavyPackages = activePackages.filter((p) => p.heavy == true);
+  activePackages = heavyPackages;
   drawPackages();
 }
-function pick(userChoice) {
-  if (userChoice == missingPackage) {
-    console.log("you win");
-  } else {
-    console.log("you lose");
-  }
+function guessLegal() {
+  let legalPackages = activePackages.filter((p) => p.illegal == false);
+  activePackages = legalPackages;
+  drawPackages();
+}
+function guessBig() {
+  let bigPackages = activePackages.filter((p) => p.big == true);
+  activePackages = bigPackages;
+  drawPackages();
+}
+function guessFragile() {
+  let fragilePackages = activePackages.filter((p) => p.fragile == true);
+  activePackages = fragilePackages;
+  drawPackages();
+}
+function guessPriority() {
+  let urgentPackages = activePackages.filter((p) => p.priority == true);
+  activePackages = urgentPackages;
+  drawPackages();
+}
+function guess(attribute) {
+  guesses++;
+  let filteredPackages = activePackages.filter(
+    (p) => p[attribute] == correctPackage[attribute]
+  );
+  activePackages = filteredPackages;
+  drawPackages();
+  drawScore();
 }
 
-// drawPackages()
-// console.log(allPackages);
+function choose(to) {
+  let found = activePackages.find((p) => p.to == to);
+  if (found.to == correctPackage.to) {
+    toast(`You found them in ${guesses} guesses`, "success");
+  } else {
+    // window.alert(`you didn't find them, now you die`)
+    toast(`you didn't find them, now you die`, "error");
+  }
+}
+function toast(title, icon) {
+  // @ts-ignore
+  Swal.fire({
+    title: title,
+    icon: icon,
+    toast: true,
+    showConfirmButton: false,
+    timer: 5000,
+    timerProgressBar: true,
+    position: "top",
+  });
+}
 startSort();
+drawPackages();
